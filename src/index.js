b1495e6744ece5b6cac1eb971f283b37c2d48439
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-03-30 10:56:57 +0800

    replaced cartHidden redux to apollo-client in header, cart-icon component and added apollo container

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { ApolloClient } from 'apollo-boost';
 
 import { store, persistor } from './redux/store';
 
+import { resolvers, typeDefs } from './graphql/resolvers';
+
 import './index.css';
 import App from './App';
 
@@ -21,7 +23,15 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
+});
+
+client.writeData({
+  data: {
+    cartHidden: true
+  }
 });
 
 ReactDOM.render(
